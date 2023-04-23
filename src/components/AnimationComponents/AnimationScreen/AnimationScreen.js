3176c6f4b871baab30a25a09f50213efd9b09ed5
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-04-23 20:31:22 -0300

    Simplicação de barra de progresso.

--- src/components/AnimationComponents/AnimationScreen/AnimationScreen.js
+++ src/components/AnimationComponents/AnimationScreen/AnimationScreen.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ButtonExpand from '../../ButtonComponents/ButtonExpand/ButtonExpand';
 import ButtonPlay from '../../ButtonComponents/ButtonPlay/ButtonPlay.js';
-import ProgressBar from '../../ButtonComponents/ProgressBar/ProgressBar';
 
 function AnimationScreen(props) {
   function getAnimationScreenClass() {
-    return `code-snippet__animation ${props?.theme === 'dark' ? 'background-color--color-blue--dark' : 'background-color--color-blue--light'}`
+    return props.theme === 'dark' ? 'background-color--color-blue--dark' : 'background-color--color-blue--light';
+  }
+
+  function getThemeClass() {
+    return props.theme === 'dark' ? 'progress-bar--light' : 'progress-bar--dark';
   }
 
   return (
-    <div className={getAnimationScreenClass()}>
+    <div className={`code-snippet__animation ${getAnimationScreenClass()}`}>
       <div className='animation-screen__screen'></div>
       <div className='animation-screen__control'>
         <ButtonPlay height='1.5rem' width='1.5rem' color={props.theme === 'dark' ? '#3498DB' : '#1E1E1E'} />
-        <ProgressBar theme={props.theme} />
+        <input className={`progress-bar ${getThemeClass()}`} type='range' min={0} max={100} step={1} defaultValue={0} />
         <ButtonExpand height='1.5rem' width='1.5rem' color={props.theme === 'dark' ? '#3498DB' : '#1E1E1E'} />
       </div>
     </div>
