9fefd98e8361b32986713da3f78da73d00815267
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-04-27 22:00:57 -0300

    Otimização em cálculo de elementos a serem colocados em tela.

--- src/components/AnimationComponents/AnimationEngine/AnimationEngine.js
+++ src/components/AnimationComponents/AnimationEngine/AnimationEngine.js
@@ -5,17 +5,18 @@ function AnimationEngine(props) {
   const [elements, setElements] = useState([]);
   const animationEngineRef = useRef(null);
 
+  const executeCommandsCallback = useCallback(executeCommands, [executeCommands]);
+
+  function executeCommands() {
+    animate.execute(props.commands);
+  }
+
+  useEffect(() => executeCommandsCallback(), [executeCommandsCallback, props.commands]);
+
   const placeObjectsCallback = useCallback(placeObjects, [placeObjects]);
 
   function placeObjects() {
     if (!props.play) { return; }
-    if (!props.commands.length) { return; }
-
-    executeCommands();
-  }
-
-  function executeCommands() {
-    animate.execute(props.commands);
 
     setElements(animate.elements);
   }
@@ -23,105 +24,7 @@ function AnimationEngine(props) {
   useEffect(placeObjectsCallback, [placeObjectsCallback]);
 
   return (
-    <div className='animation-engine no-select' ref={animationEngineRef}>
-      {elements}
-      {/* <div className='animation-engine__tree'>
-        <span className='animation-engine__node'>12</span>
-        <div className='animation-engine__children'>
-          <div className='animation-engine__subtree animation-engine__subtree--left'>
-            <span className='animation-engine__node'>11</span>
-            <div className='animation-engine__children'>
-              <div className='animation-engine__subtree animation-engine__subtree--left'>
-                <span className='animation-engine__node'>9</span>
-                <div className='animation-engine__children'>
-                  <div className='animation-engine__subtree animation-engine__subtree--left'>
-                    <span className='animation-engine__node'>8</span>
-                  </div>
-                  <div className='animation-engine__subtree animation-engine__subtree--right'>
-                    <span className='animation-engine__node'>10</span>
-                    <div className='animation-engine__children'>
-                      <div className='animation-engine__subtree animation-engine__subtree--left'>
-                        <span className='animation-engine__node'>11</span>
-                        <div className='animation-engine__children'>
-                          <div className='animation-engine__subtree animation-engine__subtree--left'>
-                            <span className='animation-engine__node'>9</span>
-                            <div className='animation-engine__children'>
-                              <div className='animation-engine__subtree animation-engine__subtree--left'>
-                                <span className='animation-engine__node'>8</span>
-                                <div className='animation-engine__children'>
-                                  <div className='animation-engine__subtree animation-engine__subtree--left'>
-                                    <span className='animation-engine__node'>11</span>
-                                    <div className='animation-engine__children'>
-                                      <div className='animation-engine__subtree animation-engine__subtree--left'>
-                                        <span className='animation-engine__node'>9</span>
-                                        <div className='animation-engine__children'>
-                                          <div className='animation-engine__subtree animation-engine__subtree--left'>
-                                            <span className='animation-engine__node'>8</span>
-                                          </div>
-                                          <div className='animation-engine__subtree animation-engine__subtree--right'>
-                                            <span className='animation-engine__node'>10</span>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              <div className='animation-engine__subtree animation-engine__subtree--right'>
-                                <span className='animation-engine__node'>10</span>
-                                <div className='animation-engine__children'>
-                                  <div className='animation-engine__subtree animation-engine__subtree--left'>
-                                    <span className='animation-engine__node'>11</span>
-                                    <div className='animation-engine__children'>
-                                      <div className='animation-engine__subtree animation-engine__subtree--left'>
-                                        <span className='animation-engine__node'>9</span>
-                                        <div className='animation-engine__children'>
-                                          <div className='animation-engine__subtree animation-engine__subtree--left'>
-                                            <span className='animation-engine__node'>8</span>
-                                          </div>
-                                          <div className='animation-engine__subtree animation-engine__subtree--right'>
-                                            <span className='animation-engine__node'>10</span>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='animation-engine__subtree animation-engine__subtree--right'>
-            <span className='animation-engine__node'>13</span>
-            <div className='animation-engine__children'>
-              <div className='animation-engine__subtree animation-engine__subtree--left'>
-                <span className='animation-engine__node'>11</span>
-                <div className='animation-engine__children'>
-                  <div className='animation-engine__subtree animation-engine__subtree--left'>
-                    <span className='animation-engine__node'>9</span>
-                    <div className='animation-engine__children'>
-                      <div className='animation-engine__subtree animation-engine__subtree--left'>
-                        <span className='animation-engine__node'>8</span>
-                      </div>
-                      <div className='animation-engine__subtree animation-engine__subtree--right'>
-                        <span className='animation-engine__node'>10</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
-    </div>
+    <div className='animation-engine no-select' ref={animationEngineRef}>{elements}</div>
   );
 }
 
