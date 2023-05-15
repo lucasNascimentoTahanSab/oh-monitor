a4d89a5b49208f63f6e4e15d1a135661088db63a
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-05-14 22:37:14 -0300

    Definição de RenderContext.

---
+++ src/components/Context/RenderContext/RenderContext.js
@@ -0,0 +1,9 @@
+/**
+ * @file Módulo responsável pela criação do contexto RenderContext para indicar momento de renderização.
+ * @copyright Lucas N. T. Sab 2023
+ */
+import { createContext } from 'react';
+
+const RenderContext = createContext(null);
+
+export default RenderContext;
