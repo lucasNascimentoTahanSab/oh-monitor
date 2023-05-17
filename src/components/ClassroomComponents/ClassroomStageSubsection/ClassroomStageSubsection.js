ced11ccf53c5377a968b0490613a4a0b94cf2b54
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-05-17 01:05:02 -0300

    Configuração de subseção.

--- src/components/ClassroomComponents/ClassroomStageSubsection/ClassroomStageSubsection.js
+++ src/components/ClassroomComponents/ClassroomStageSubsection/ClassroomStageSubsection.js
@@ -2,10 +2,14 @@
  * @file Módulo responsável pela exibição da seção atual e seus respectivos elementos.
  * @copyright Lucas N. T. Sab 2023
  */
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import SubsecionContext from '../../Context/SubsectionContext/SubsectionContext.js';
 import Builder from '../../../classes/util/Builder.js';
+import SectionContext from '../../Context/SectionContext/SectionContext.js';
+import Util from '../../../classes/util/Util.js';
 
 function ClassroomStageSubsection(props) {
+  const [section, setSection] = useContext(SectionContext);
   const [subsection, setSubsection] = useState(null);
 
   useEffect(() => setSubsection(props.subsection), [props.subsection]);
@@ -21,13 +25,25 @@ function ClassroomStageSubsection(props) {
     return subsection.elements.map(element => Builder.getElement(element));
   }
 
+  /**
+   * Método responsável pela atualização da subseção atual assim como sua correspondente
+   * em seção.
+   * 
+   * @param {object} subsection 
+   */
+  function updateSubsection(subsection) {
+    Util.setCurrentItem(section, setSection)(subsection);
+  }
+
   return (
-    <section id={subsection?.uid} className='classroom__content-section'>
-      <h3>{subsection?.title}</h3>
-      <div className='classroom__content-section'>
-        {getElements()}
-      </div>
-    </section>
+    <SubsecionContext.Provider value={[subsection, updateSubsection]}>
+      <section id={subsection?.uid} className='classroom__content-section'>
+        <h3>{subsection?.title}</h3>
+        <div className='classroom__content-section'>
+          {getElements()}
+        </div>
+      </section>
+    </SubsecionContext.Provider>
   );
 }
 
---
+++ src/components/Context/SubsectionContext/SubsectionContext.js
@@ -0,0 +1,10 @@
+/**
+ * @file Módulo responsável pela criação do contexto SubsectionContext para transmissão de informações 
+ * quanto à subseção atual.
+ * @copyright Lucas N. T. Sab 2023
+ */
+import { createContext } from 'react';
+
+const SubsectionContext = createContext(null);
+
+export default SubsectionContext;
