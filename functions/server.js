645c4e416ee29ec8857605139449ce641cbcd315
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-05-25 13:32:37 -0300

    Deploy de functions.

---
+++ functions/server.js
@@ -0,0 +1,25 @@
+/**
+ * @file Módulo responsável pelo intermédio de comunicações backend da aplicação com serviços externos.
+ * @copyright Lucas N. T. Sab 2023 
+ */
+const express = require('express');
+const path = require('path');
+const CX = require('../routes/codeX/codeXRouter');
+const ST = require('../routes/strapi/strapiRouter');
+const GH = require('../routes/gitHub/gitHubRouter');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+app.use('/api/code', CX.router);
+app.use('/api/content', ST.router);
+app.use('/api/repo', GH.router);
+
+app.listen(process.env.PORT);
+
+module.exports = app;
