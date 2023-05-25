62d982afd612b31d1a51a037c3338750c9600810
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-05-25 15:22:17 -0300

    Provendo React app.

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 app.use('/api/code', CX.router);
 app.use('/api/content', ST.router);
